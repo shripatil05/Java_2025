81bd76b50bb9a277a1ae0a83b21d350c93ce4aa2
shripatil05 <shrivardhan05@gmail.com>
2025-11-13 12:12:01 +0530

    abc

--- com/prowings/Java2025/ObjectClass/copyconstructor/deep/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.prowings.Java2025.ObjectClass.copyconstructor.deep;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("cjdsbvdfhdgkjde");
-	}
-
-}
